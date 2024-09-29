a1c7917ea13cb99663aa6a35e410f60841bc7c04
Lucas Rosén <49569378+LucasRosen@users.noreply.github.com>
2024-09-29 20:24:01 +0200

    Add generate grid button to clear the current grid and generate a new one with given size

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const gridContainer = document.querySelector("#container");
+const newGridButton = document.querySelector("#new-grid-button");
 
 // Generates a gridSize x gridSize grid of divs
 function generateGrid(gridSize) {
@@ -11,6 +12,14 @@ function generateGrid(gridSize) {
       const newSquare = createSquare();
       newColumn.appendChild(newSquare);
     }
+  }
+}
+
+function clearGrid() {
+  let child = gridContainer.lastElementChild;
+  while (child) {
+    gridContainer.removeChild(child);
+    child = gridContainer.lastElementChild;
   }
 }
 
@@ -33,5 +42,22 @@ function onMouseOut(event) {
   const targetSquare = event.target;
   targetSquare.style.cssText = "opacity: 100%";
 }
+
+function isNumber(value) {
+  return typeof value === "number";
+}
+
+// Generate new grid when button is clicked
+newGridButton.addEventListener("click", () => {
+  const newGridSize = Number(
+    prompt("Enter new grid size:\nMust be a number between 10-100", 16)
+  );
+  if (isNumber(newGridSize) && newGridSize >= 10 && newGridSize <= 100) {
+    clearGrid();
+    generateGrid(newGridSize);
+  } else {
+    alert("Invalid grid size");
+  }
+});
 
 generateGrid(16);
